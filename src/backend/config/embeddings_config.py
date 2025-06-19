5b66028be2a13470b7ba026caded3fc02b264c50
Emilija Gjorgjevska <48652639+martech-engineer@users.noreply.github.com>
2025-06-19 20:49:11 +0200

    Update embeddings_config.py

--- src/backend/config/embeddings_config.py
+++ src/backend/config/embeddings_config.py
@@ -8,5 +8,5 @@ class EmbeddingConfig:
         "/content/drive/My Drive/WebKnoGraph/data/crawled_data_parquet/"
     )
     output_path: str = "/content/drive/My Drive/WebKnoGraph/data/url_embeddings/"
-    model_name: str = "all-MiniLM-L6-v2"  # should be changed to other/multilingual content if the content is not in English
+    model_name: str = "all-MiniLM-L6-v2"  # should be changed to other/multilingual content if the content is not in English or longer
     batch_size: int = 10

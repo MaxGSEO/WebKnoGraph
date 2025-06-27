054257dbe3b66305e388b3dc3583d11956e0a775
Emilija Gjorgjevska <emmagjorgjevska@gmail.com>
2025-06-27 23:47:29 +0200

    Fixed dead code in embeddings config

--- src/backend/config/embeddings_config.py
+++ src/backend/config/embeddings_config.py
@@ -8,4 +8,4 @@ class EmbeddingConfig:
     input_path: str = "/content/drive/My Drive/WebKnoGraph/data/crawled_data_parquet/"
     output_path: str = "/content/drive/My Drive/WebKnoGraph/data/url_embeddings/"
     model_name: str = "nomic-ai/nomic-embed-text-v1.5"  # should be changed to other model if the content is non-English or longer than 8192 tokens
-    batch_size: int = 10
+    batch_size: int = 1
